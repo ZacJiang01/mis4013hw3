704a6619b74a6f814dd03e97cfb21d1695a9cf04
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-10-02 12:30:03 -0500

    Create courses.php

---
+++ courses.php
@@ -0,0 +1,11 @@
+<?php 
+require_once("util-db.php");
+require_once("model-courses.php");
+
+$pageTitle = "Courses";
+include "view-header.php";
+$instructors = selectCourses();
+
+include "view-instructors.php";
+include "view-footer.php";
+?>
