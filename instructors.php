b77d2aa633bf3ab4bf6771deff08b063df8f1eb3
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-10-01 21:44:55 -0500

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -1,1 +1,11 @@
+<?php 
+require_once("util-db.php");
+require_once("model-instructors.php");
 
+$pageTitle = "Instructors";
+include "view-header.php";
+$instructors = selectInstructors();
+
+include "view-instructors.php";
+include "view-footer.php";
+?>
