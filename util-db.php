e8719cebe898e0339682a7eb20f9120aba828f04
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-10-02 00:20:26 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,13 +1,25 @@
 <?php
-function get_db_connection(){
+function get_db_connection() {
     // Create connection
     $conn = new mysqli(
-'mis4013hw3database.mysql.database.azure.com', 'jian0040', 'Beans1310098965!!', 'my_database');
-    
+        'mis4013hw3database.mysql.database.azure.com',
+        'jian0040',
+        'Beans1310098965!!',
+        'my_database'
+    );
+
     // Check connection
     if ($conn->connect_error) {
-      return false;
+        // Log error message
+        error_log("Connection failed: " . $conn->connect_error);
+        return false; // or you can throw an exception
     }
+    
+    // Set character set to utf8mb4 for better Unicode support
+    if (!$conn->set_charset("utf8mb4")) {
+        error_log("Error loading character set utf8mb4: " . $conn->error);
+    }
+
     return $conn;
 }
 ?>
