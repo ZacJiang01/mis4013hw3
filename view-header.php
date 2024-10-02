e7bbf3fd0af172ce53b08940d0f5a5bdff5a290f
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-10-01 21:36:03 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container"> <!-- Fixed spacing -->
+    <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Navbar</a>
@@ -20,16 +20,13 @@
                 <a class="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="post.php">Post</a>
+                <a class="nav-link" href="Instructors.php">Instructors</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="post-result.php">Post Result</a>
+                <a class="nav-link" href="courses.php">Courses</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="get.php">Get</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="get-result.php">Get Result</a>
+                <a class="nav-link" href="instrcutors-with-courses.php">Instructors with Courses</a>
               </li>
             </ul>
           </div>
