00ad828329d93d71dcc19642687e02295fb01f24
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-10-02 12:37:16 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -1,15 +1,30 @@
-<? php
+<?php
 function selectCourses() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT course_id, course_name, course_number FROM 'course'");
+        
+        // Correct the SQL query by removing single quotes around the table name
+        $stmt = $conn->prepare("SELECT course_id, course_name, course_number FROM course");
+        
+        // Execute the statement
         $stmt->execute();
+        
+        // Get the result
         $result = $stmt->get_result();
+        
+        // Close the connection after use
         $conn->close();
+        
+        // Return the result
         return $result;
     } catch (Exception $e) {
-        $conn->close();
+        // Close the connection in case of an error
+        if (isset($conn)) {
+            $conn->close();
+        }
+        
+        // Rethrow the exception to be handled elsewhere
         throw $e;
     }
 }
-  ?>
+?>
