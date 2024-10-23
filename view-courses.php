f4c998e8a72a7e877b72be4189ec464bd0692452
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-10-23 10:37:59 -0500

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -1,4 +1,4 @@
-<h1>Instructors</h1>
+<h1>Car Makes</h1>
 <div class="table-responsive">
   <table class="table">
    <thead>
@@ -13,13 +13,32 @@
 while ($course = $courses->fetch_assoc()) {
   ?>
   <tr>
-    <td><?php echo $instructor['course_id']; ?></td>
-     <td><?php echo $instructor['course_name']; ?></td>
-     <td><?php echo $instructor['course_number']; ?></td>
+    <td><?php echo $instructor['make_id']; ?></td>
+     <td><?php echo $instructor['make_name']; ?></td>
+     <td><?php echo $instructor['make_country']; ?></td>
   </tr>
   <?php
 }
 ?>
     </tbody>
   </table>
+</div>
+
+<div id="addEditPopup" class="popup" style="display:none;">
+    <form action="add_edit_car.php" method="POST">
+        <h2>Add/Edit Car Make</h2>
+        <label for="make">Make:</label>
+        <input type="text" name="make_name" value="" required><br>
+        <label for="country">Country:</label>
+        <input type="text" name="make_country" value="" required><br>
+        <label for="model">Model:</label>
+        <input type="text" name="model_name" value="" required><br>
+        <label for="year">Year:</label>
+        <input type="number" name="model_year" value="" required><br>
+        <button type="submit">Submit</button>
+    </form>
+</div>
+
+<div id="notification" class="notification" style="display:none;">
+    <p>Car Make operation was successful!</p>
 </div>
