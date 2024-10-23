56a8c51839a6b2bfa35c06595d3f314156010340
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-10-23 10:36:56 -0500

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -2,9 +2,9 @@
 require_once("util-db.php");
 require_once("model-instructors.php");
 
-$pageTitle = "Instructors";
+$pageTitle = "Car Models";
 include "view-header.php";
-$instructors = selectInstructors();
+$instructors = selectCar Models();
 
 include "view-instructors.php";
 include "view-footer.php";
