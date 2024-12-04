0bfc07c523ef4a302e999e891a6e7693b0438b14
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-03 21:24:39 -0600

    Update car.php

--- car.php
+++ car.php
@@ -1,6 +1,17 @@
 <?php 
 require_once("util-db.php");
-require_once("selectcar.php");
+
+try {
+    $cars = selectCars();
+    foreach ($cars as $car) {
+        echo "ID: " . htmlspecialchars($car['CarID']) . ", ";
+        echo "Model: " . htmlspecialchars($car['CarModel']) . ", ";
+        echo "Color: " . htmlspecialchars($car['Color']) . ", ";
+        echo "Price: $" . htmlspecialchars(number_format($car['Price'], 2)) . "<br>";
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
 
 $pageTitle = "Car Models";
 include "view-header.php";
