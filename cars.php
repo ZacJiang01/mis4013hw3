c5ea0dc085406b55e4b95fd785eb881312f1a823
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-04 12:41:17 -0600

    Update cars.php

--- cars.php
+++ cars.php
@@ -6,6 +6,13 @@ $pageTitle = "Cars";
 include "view-header.php";
 $Cars = selectCars();
 
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType'])) {
+    case "Add";
+      insertCars($_POST['cCarModel'], $_POST['cColor'], $_POST['cPrice']
+          break;
+  }
+}
 include "view-cars.php";
 include "view-footer.php";
 ?>
