5e4ceb0b62319f947fed050ec206edc677f94a43
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-03 22:01:20 -0600

    Create selectcombined.php

---
+++ selectcombined.php
@@ -0,0 +1,31 @@
+<?php
+require_once("util-db.php");
+
+function selectCombinedData() {
+    try {
+        $conn = get_db_connection();
+
+        // Query to fetch manufacturers and their cars
+        $query = "
+            SELECT Manufacturer.ManufacturerID, Manufacturer.ManufacturerName, 
+                   Car.CarID, Car.CarModel, Car.Color, Car.Price
+            FROM Manufacturer
+            LEFT JOIN Car ON Manufacturer.ManufacturerID = Car.ManufacturerID
+            ORDER BY Manufacturer.ManufacturerName ASC, Car.CarID ASC
+        ";
+        $result = $conn->query($query);
+
+        if (!$result) {
+            throw new Exception("Query execution failed: " . $conn->error);
+        }
+
+        return $result;
+    } catch (Exception $e) {
+        if (isset($conn)) {
+            $conn->close();
+        }
+        error_log("Error in selectCombinedData(): " . $e->getMessage());
+        throw $e;
+    }
+}
+?>
