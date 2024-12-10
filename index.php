9dd06554ecad2d237f4547d623547696ab85d704
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-10 17:25:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,46 +10,7 @@ include "view-header.php";
         <a href="cars.php" class="btn btn-primary btn-lg">View Inventory</a>
     </div>
 
-    <!-- Featured Cars Section -->
-    <div class="my-5">
-        <h2 class="text-center mb-4">Featured Cars</h2>
-        <div class="row">
-            <!-- Car Card 1 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="images/car1.jpg" class="card-img-top" alt="Car 1">
-                    <div class="card-body">
-                        <h5 class="card-title">2024 Toyota Camry</h5>
-                        <p class="card-text">Color: White<br>Price: $25,000</p>
-                        <a href="car-details.php?id=1" class="btn btn-primary">View Details</a>
-                    </div>
-                </div>
-            </div>
-            <!-- Car Card 2 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="images/car2.jpg" class="card-img-top" alt="Car 2">
-                    <div class="card-body">
-                        <h5 class="card-title">2024 Ford Mustang</h5>
-                        <p class="card-text">Color: Red<br>Price: $35,000</p>
-                        <a href="car-details.php?id=2" class="btn btn-primary">View Details</a>
-                    </div>
-                </div>
-            </div>
-            <!-- Car Card 3 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="images/car3.jpg" class="card-img-top" alt="Car 3">
-                    <div class="card-body">
-                        <h5 class="card-title">2024 Honda Civic</h5>
-                        <p class="card-text">Color: Blue<br>Price: $22,000</p>
-                        <a href="car-details.php?id=3" class="btn btn-primary">View Details</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
+  
     <!-- Contact Section -->
     <div class="bg-primary text-white text-center py-5 rounded">
         <h2>Contact Us</h2>
