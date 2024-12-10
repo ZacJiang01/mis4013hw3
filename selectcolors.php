0b60858a12edb90b42c379809f3a4a7ad7ec58b5
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-10 16:55:26 -0600

    Update selectcolors.php

--- selectcolors.php
+++ selectcolors.php
@@ -1,1 +1,28 @@
+<?php
+require_once("util-db.php");
 
+function selectColors() {
+    try {
+        $conn = get_db_connection();
+
+        // Query to fetch only car details
+        $query = "
+            SELECT Color
+            FROM car 
+        ";
+        $result = $conn->query($query);
+
+        if (!$result) {
+            throw new Exception("Query execution failed: " . $conn->error);
+        }
+
+        return $result;
+    } catch (Exception $e) {
+        if (isset($conn)) {
+            $conn->close();
+        }
+        error_log("Error in selectColors(): " . $e->getMessage());
+        throw $e;
+    }
+}
+?>
