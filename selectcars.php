a6cac493360f769d64cd0059b5909f4761f61cd1
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-10 14:20:27 -0600

    Update selectcars.php

--- selectcars.php
+++ selectcars.php
@@ -7,8 +7,8 @@ function selectCars() {
 
         // Query to fetch only car details
         $query = "
-            SELECT CarID, CarModel, Color, Price
-            FROM Car
+            SELECT CarID, CarModel, Color, Price, ManufacturerName
+            FROM car c join manufacturer m on c.ManufacturerID = m.ManufacturerID;
             ORDER BY CarID ASC
         ";
         $result = $conn->query($query);
