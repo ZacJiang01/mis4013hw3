ed24154dc50e382922ddf9ceeaab7741b8f8e4de
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-10 14:31:27 -0600

    Update view-carmanufacturer.php

--- view-carmanufacturer.php
+++ view-carmanufacturer.php
@@ -5,19 +5,27 @@
       <tr>
         <th>ID</th>
         <th>Name</th>
+        <th>View Cars</th>
       </tr>
     </thead>
     <tbody>
       <?php
       if ($Manufacturers && $Manufacturers->num_rows > 0) {
           while ($manufacturer_row = $Manufacturers->fetch_assoc()) {
-              echo "<tr>";
-              echo "<td>" . htmlspecialchars($manufacturer_row['ManufacturerID']) . "</td>";
-              echo "<td>" . htmlspecialchars($manufacturer_row['ManufacturerName']) . "</td>";
-              echo "</tr>";
+      ?>
+      <tr>
+        <td><?php echo htmlspecialchars($manufacturer_row['ManufacturerID']); ?></td>
+        <td><?php echo htmlspecialchars($manufacturer_row['ManufacturerName']); ?></td>
+        <td>
+          <a href="manufacturer_cars.php?manufacturer=<?php echo urlencode($manufacturer_row['ManufacturerName']); ?>">
+            View Cars
+          </a>
+        </td>
+      </tr>
+      <?php
           }
       } else {
-          echo "<tr><td colspan='2'>No manufacturers found or query failed.</td></tr>";
+          echo "<tr><td colspan='3'>No manufacturers found or query failed.</td></tr>";
       }
       ?>
     </tbody>
