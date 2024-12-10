3954d96e72834e0f0634a5ca19a1ab23b4c536ae
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-10 17:09:14 -0600

    Update cars-by-colors.php

--- cars-by-colors.php
+++ cars-by-colors.php
@@ -4,7 +4,7 @@ require_once("model-cars-by-colors.php");
 
 $pageTitle = "Cars by Colors";
 include "view-header.php";
-$Cars = selectCarsByColors($POST['cid']);
+$Cars = selectCarsByColors($_POST['cid']);
 
 include "view-cars-by-colors.php";
 include "view-footer.php";
