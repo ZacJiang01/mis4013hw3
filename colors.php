c437988930c942b008c87b4c6abd94c407552f0b
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-10 16:53:59 -0600

    Update colors.php

--- colors.php
+++ colors.php
@@ -1,54 +1,11 @@
-<?php
+<?php 
 require_once("util-db.php");
+require_once("selectcars.php");
 
-$pageTitle = "Car Colors";
-
-// Query to get cars grouped by color in alphabetical order
-$conn = get_db_connection();
-$query = "
-    SELECT Car.Color
-";
-$result = $conn->query($query);
-
+$pageTitle = "Colors";
 include "view-header.php";
-?>
-<h1>Car Colors</h1>
-<div class="table-responsive">
-  <?php if ($result && $result->num_rows > 0): ?>
-    <?php 
-    $currentColor = null; 
-    while ($row = $result->fetch_assoc()): 
-        if ($row['Color'] !== $currentColor): 
-            if ($currentColor !== null): ?>
-              </tbody></table>
-            <?php endif; ?>
-            <h2><?= htmlspecialchars($row['Color']); ?></h2>
-            <table class="table">
-              <thead>
-                <tr>
-                  <th>Manufacturer</th>
-                  <th>Model</th>
-                  <th>Price</th>
-                </tr>
-              </thead>
-              <tbody>
-            <?php 
-            $currentColor = $row['Color']; 
-        endif; 
-        ?>
-        <tr>
-          <td><?= htmlspecialchars($row['ManufacturerName']); ?></td>
-          <td><?= htmlspecialchars($row['CarModel']); ?></td>
-          <td>$<?= number_format($row['Price'], 2); ?></td>
-        </tr>
-    <?php endwhile; ?>
-    </tbody>
-    </table>
-  <?php else: ?>
-    <p>No cars found.</p>
-  <?php endif; ?>
-</div>
-<?php
-$conn->close();
+$Cars = selectCars();
+
+include "view-cars.php";
 include "view-footer.php";
 ?>
