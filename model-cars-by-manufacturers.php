d84161444f2f07481c95d7effa3b6dd28e596daf
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-10 14:49:43 -0600

    Update model-cars-by-manufacturers.php

--- model-cars-by-manufacturers.php
+++ model-cars-by-manufacturers.php
@@ -2,4 +2,16 @@
 function SelectCarsByManufacturers($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare(SELECT car.id, car.
+        $stmt = $conn->prepare("SELECT CarId, CarModel, Color, Price, FROM Car c join manufacturer m on c.manufacturerid = m.manufacturerid where carid=?");
+        $stmt->bind_param("id", $iid);
+        $stmt-?execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+        } catch (Exception $e) {
+            $conn->close();
+            throw $e;
+            }
+}
+?>
+        
