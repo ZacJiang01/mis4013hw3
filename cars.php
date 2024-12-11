5f9918bb1b5384d64be9fe307372a18e7485fc66
ZacJiang01 <107667231+ZacJiang01@users.noreply.github.com>
2024-12-10 22:32:21 -0600

    Update cars.php

--- cars.php
+++ cars.php
@@ -17,9 +17,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['actionType'])) {
                 $error_message = "All fields are required, and price must be a positive number.";
             } else {
                 if (!insertCar($carModel, $color, $price)) {
-                    $error_message = "Failed to add car.";
+                    echo '<div class ="alert-danger" role ="alert">Error.</div>';
                 } else {
-                    $success_message = "Car added successfully!";
+                    echo '<div class = "alert alert-success" role ="alert">New Car Added.</div>';
                 }
             }
             break;
